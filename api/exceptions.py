3f9fe6ffe666d5d922426dd0d8f20351aa5b6ec0
Christ Mboungou <cmboungou1@live.maryville.com>
2022-03-28 09:13:19 -0400

    3

--- api/exceptions.py
+++ api/exceptions.py
@@ -6,6 +6,6 @@ def my_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
         response.data["message"] = str(exc)
-        #response.data["results"] = None
+        response.data["results"] = exc.get_full_details()
         #del response.data["detail"]
     return response
