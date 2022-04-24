b5e591d16696badec3ffafaf1732c8ed47d55e03
Christ Mboungou <cmboungou1@live.maryville.com>
2022-04-23 22:00:05 -0400

    4 commit

--- goal_setter/settings.py
+++ goal_setter/settings.py
@@ -48,17 +48,31 @@ INSTALLED_APPS = [
     "base.apps.BaseConfig",
     "api.apps.ApiConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    # "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 ROOT_URLCONF = "goal_setter.urls"
