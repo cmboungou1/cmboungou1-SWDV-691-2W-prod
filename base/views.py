131d9350fe90cd4ea06c3e5781c491e76890339e
Christ Mboungou <cmboungou1@live.maryville.com>
2022-04-28 03:47:50 -0400

    Impact almost done

--- base/views.py
+++ base/views.py
@@ -82,92 +82,130 @@ class Timeline(LoginRequiredMixin, ListView):
     model = Goal
     context_object_name = "goal"
     template_name = "timeline.html"
-
-    def get_neg_impact(self):
-        user_id = self.request.user.id
-        queryset= Goal.objects.all()
-        goals = queryset.filter(user=user_id)
-        return goals
-
-    def get_pos_impact(self):
-        Goal.objects.all()
-        return "We made it"
+    neg_gpa_print = ""
+    neg_sat_print = ""
+    neg_tutor_print = ""
+    neg_strategy_print = ""
+    pos_gpa_print = ""
+    pos_sat_print = ""
+    pos_tutor_print = ""
+    pos_strategy_print = ""
+    ave_gpa_print = ""
+    ave_sat_print = ""
+    ave_tutor_print = ""
+    ave_strategy_print = ""
+    total_sat = 0
+    total_gpa = 0
+    total_private_tutor_time = 0
+    total_have_a_strategy = 0
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['neg_impact'] = self.get_neg_impact()
-        context['pos_impact'] = self.get_pos_impact()
+        user_id = self.request.user.id
+        queryset = Goal.objects.all()
+        goals = queryset.filter(user=user_id) 
+        def get_neg_impact(category):
+            # gpa negative
+            if category == "GPA":
+                self.neg_gpa_print += ""+self.neg_gpa_print+" negative GPA"
+            # sat negative
+            if category == "SAT":
+                self.neg_sat_print += ""+self.neg_sat_print+" negative SAT"
+            # tutor negative
+            if category == "TUTOR":
+                self.neg_tutor_print += ""+self.neg_tutor_print+" negative TUTOR"
+            # strategy negative 
+            if category == "STRATEGY":
+                self.neg_strategy_print += ""+self.neg_strategy_print+" negative STRATEGY"
+        def get_pos_impact(category):
+            # gpa positive
+            if category == "GPA":
+                self.pos_gpa_print += ""+self.pos_gpa_print+" positive GPA"
+            # sat positive
+            if category == "SAT":
+                self.pos_sat_print += ""+self.pos_sat_print+" positive SAT"
+            # tutor positive
+            if category == "TUTOR":
+                self.pos_tutor_print += ""+self.pos_tutor_print+" positive TUTOR"
+            # strategy positive 
+            if category == "STRATEGY":
+                self.pos_strategy_print += ""+self.pos_strategy_print+" positive STRATEGY"
+        def get_avrg_impact(category):
+            # gpa average
+            if category == "GPA":
+                self.ave_gpa_print += ""+str(self.ave_gpa_print)+" average GPA"
+            # sat average
+            if category == "SAT":
+                self.ave_sat_print += ""+str(self.ave_sat_print)+" average SAT"
+            # tutor average
+            if category == "TUTOR":
+                self.ave_tutor_print += ""+self.ave_tutor_print+" average TUTOR"
+        def impactMetrics():           
+            gpa = 0
+            sat = 0
+            private_tutor_time = 0
+            i = 0
+            a = 0
+            b = 0
+            c = 0
+            for goal in goals:
+                if goal.category == "GPA":
+                    i += 1
+                    print(goal)
+                    gpa += goal.gpa.current_gpa      
+                if goal.category == "SAT":
+                    print(goal)
+                    a += 1
+                    sat += goal.sat.practice_test_score
+                    b += 1
+                    private_tutor_time += goal.sat.private_tutor_time
+                    c += 1
+                    print("Have a strategy", goal.sat.have_a_strategy)
+                    if goal.sat.have_a_strategy == True :
+                        self.total_have_a_strategy += 1 
+            # calculate the overall GPA
+            self.total_gpa = gpa / i
+            if self.total_gpa < 3.0 :
+                print("3.0 this")
+                get_neg_impact("GPA")
+            elif self.total_sat >= 3.0 and self.total_sat <= 3.5 :
+                get_avrg_impact("GPA") 
+            elif self.total_sat >= 3.5 :
+                print("3.5")
+                get_pos_impact("GPA")
+            # practice sat score metrics
+            self.total_sat = sat / a
+            if self.total_sat < 1040 :
+                print("Sat99")
+                get_neg_impact("SAT")
+            elif self.total_sat >= 1040 and self.total_sat <= 1060 :
+                get_avrg_impact("SAT") 
+            elif self.total_sat > 1060:
+                get_pos_impact("SAT")
+            # private tutor hours
+            self.total_private_tutor_time = private_tutor_time / b
+            if self.total_private_tutor_time < 10 and self.total_sat < 1040 :
+                get_neg_impact("TUTOR")
+            elif self.total_sat >= 1040 and self.total_sat <= 1060 :
+                get_avrg_impact("TUTOR") 
+            elif self.total_sat > 1060 and self.total_private_tutor_time >= 80 :
+                get_pos_impact("TUTOR")
+            # do you have a strategy
+            if self.total_have_a_strategy >= 1 :
+                get_pos_impact("STRATEGY")
+            else :
+                get_neg_impact("STRATEGY") 
+                    
+        impactMetrics()
+        print("hereeee", self.ave_sat_print)            
+        context['neg_impact'] = ""+self.neg_gpa_print+""+self.neg_sat_print+""
+        context['pos_impact'] = ""+self.pos_gpa_print+""+self.pos_sat_print+""
+        context['ave_impact'] = ""+self.ave_gpa_print+""+self.ave_sat_print+""
+
+        context['neg_impact'] += ""+self.neg_tutor_print+""+self.neg_strategy_print+""
+        context['pos_impact'] += ""+self.pos_tutor_print+""+self.pos_strategy_print+""
+        context['ave_impact'] += ""+self.ave_tutor_print+""+self.ave_strategy_print+""
+
         return context
-# class RegisterPage(FormView):
-#     template_name = "base/register.html"
-#     form_class = UserCreationForm
-#     redirect_authenticated_user = True
-#     success_url = reverse_lazy("goals")
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         if user is not None:
-#             login(self.request, user)
-#         return super(RegisterPage, self).form_valid(form)
-
-#     def get(self, *args, **kwargs):
-#         if self.request.user.is_authenticated:
-#             return redirect("goals")
-#         return super(RegisterPage, self).get(*args, **kwargs)
-
-# class CustomLoginView(LoginView):
-#     template_name = "base/login.html"
-#     fields = "__all__"
-#     redirect_authenticated_user = True
-
-#     def get_success_url(self):
-#         return reverse_lazy("goals")
-
-
-# class RegisterPage(FormView):
-#     template_name = "base/register.html"
-#     form_class = UserCreationForm
-#     redirect_authenticated_user = True
-#     success_url = reverse_lazy("goals")
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         if user is not None:
-#             login(self.request, user)
-#         return super(RegisterPage, self).form_valid(form)
-
-#     def get(self, *args, **kwargs):
-#         if self.request.user.is_authenticated:
-#             return redirect("goals")
-#         return super(RegisterPage, self).get(*args, **kwargs)
-
-
-# class GoalList(LoginRequiredMixin, ListView):
-#     model = Goal
-#     context_object_name = "goals"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["goals"] = context["goals"].filter(user=self.request.user)
-#         context["count"] = context["goals"].filter(complete=False).count()
-#         search_input = self.request.GET.get("search-area") or ""
-#         if search_input:
-#             context["goals"] = context["goals"].filter(title__startswith=search_input)
-
-#         context["search_input"] = search_input
-
-#         return context
-
-
-
-# class GoalCreate(LoginRequiredMixin, CreateView):
-#     model = Goal
-#     fields = ["title", "description", "complete"]
-#     success_url = reverse_lazy("goals")
-
-#     def form_valid(self, form):
-#         form.instance.user = self.request.user
-#         return super(GoalCreate, self).form_valid(form)
-
-
 
